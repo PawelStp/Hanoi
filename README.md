2dd85adf1dbf7f05dc1cd8f2df4f79a15c2b0aa2
pawelstp <pawelstypko@vp.pl>
2017-04-12 13:41:12 +0200

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Hanoi
+# Hanoi
---
+++ hanoi/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace hanoi
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    private bool CommandBinding_CanExecute(object sender, ExecutedRoutedEventArgs e)
+    {
+        return true;
+    }
+    public class HanoiCommands
+    {
+        private static RoutedUICommand gora;
+        static HanoiCommands()
+        {
+            gora = new RoutedUICommand(
+            "Zdejmij krążek", "Zdejmij krązek",
+            typeof(HanoiCommands));
+        }
+        public static RoutedUICommand Gora
+        {
+            get { return gora; }
+        }
+       
+    }
+
+}
